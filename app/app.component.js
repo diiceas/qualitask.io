b60f04f397d2972f129fb920ac9a07b5c7fdc1db
diiceas <forthepost@gmail.com>
2016-07-18 15:39:36 +0300

    implement functionality to add resources, export html and css to a separate template

--- app/app.component.js
+++ app/app.component.js
@@ -11,17 +11,27 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var ng2_dragula_1 = require('ng2-dragula/ng2-dragula');
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(dragulaService) {
+        this.dragulaService = dragulaService;
+        this.many = [];
+        this.many2 = [];
+        dragulaService.setOptions('another-bag', {
+            copy: true,
+            copySortSource: true,
+        });
     }
+    AppComponent.prototype.addNewTodo = function (newTodo) {
+        this.many.push(newTodo);
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>My First Angular 2 App</h1>\n      <div>\n        <div class='wrapper'>\n          <div class='container' [dragula]='\"first-bag\"'>\n            <div>Drag/drop item 1</div>\n          </div>\n          <div class='container' [dragula]='\"first-bag\"'>\n            <div>Drag/drop item 2</div>\n          </div>\n        </div>\n      </div>\n    ",
+            templateUrl: 'app/app.component.html',
             directives: [ng2_dragula_1.Dragula],
             viewProviders: [ng2_dragula_1.DragulaService],
-            styles: ["\n    .wrapper {\n      display: table;\n    }\n    .container {\n      display: table-cell;\n      background-color: rgba(255, 255, 255, 0.2);\n      width: 50%;\n    }\n    .container:nth-child(odd) {\n      background-color: rgba(0, 0, 0, 0.2);\n    }\n    .container div,\n    .gu-mirror {\n      margin: 10px;\n      padding: 10px;\n      background-color: rgba(0, 0, 0, 0.2);\n      transition: opacity 0.4s ease-in-out;\n    }\n    .container div {\n      cursor: move;\n      cursor: grab;\n      cursor: -moz-grab;\n      cursor: -webkit-grab;\n    }\n    .gu-mirror {\n      cursor: grabbing;\n      cursor: -moz-grabbing;\n      cursor: -webkit-grabbing;\n    }\n    "]
+            styleUrls: ['app/app.component.css'],
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [ng2_dragula_1.DragulaService])
     ], AppComponent);
     return AppComponent;
 }());
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,51 +3,27 @@ import { Component } from '@angular/core';
 import {Dragula, DragulaService} from 'ng2-dragula/ng2-dragula';
 
 @Component({
-    selector: 'my-app',
-    template: `<h1>My First Angular 2 App</h1>
-      <div>
-        <div class='wrapper'>
-          <div class='container' [dragula]='"first-bag"'>
-            <div>Drag/drop item 1</div>
-          </div>
-          <div class='container' [dragula]='"first-bag"'>
-            <div>Drag/drop item 2</div>
-          </div>
-        </div>
-      </div>
-    `,
-    directives: [Dragula],
-    viewProviders: [DragulaService],
-    styles: [`
-    .wrapper {
-      display: table;
-    }
-    .container {
-      display: table-cell;
-      background-color: rgba(255, 255, 255, 0.2);
-      width: 50%;
-    }
-    .container:nth-child(odd) {
-      background-color: rgba(0, 0, 0, 0.2);
-    }
-    .container div,
-    .gu-mirror {
-      margin: 10px;
-      padding: 10px;
-      background-color: rgba(0, 0, 0, 0.2);
-      transition: opacity 0.4s ease-in-out;
-    }
-    .container div {
-      cursor: move;
-      cursor: grab;
-      cursor: -moz-grab;
-      cursor: -webkit-grab;
-    }
-    .gu-mirror {
-      cursor: grabbing;
-      cursor: -moz-grabbing;
-      cursor: -webkit-grabbing;
-    }
-    `]
+  selector: 'my-app',
+  templateUrl: 'app/app.component.html',
+  directives: [Dragula],
+  viewProviders: [DragulaService],
+  styleUrls: ['app/app.component.css'],
 })
-export class AppComponent { }
+export class AppComponent {
+  newTodo: string;
+  
+
+  constructor(private dragulaService: DragulaService) {
+    dragulaService.setOptions('another-bag', {
+      copy: true,
+      copySortSource: true,             // elements in copy-source containers can be reordered
+    });    
+  }
+
+  addNewTodo(newTodo: string){
+    this.many.push(newTodo);
+  }
+
+  public many: Array<string> = [];
+  public many2: Array<string> = [];
+}
